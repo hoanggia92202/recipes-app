e8bf059611cd94cc486f089a099677f5919165bf
duc le <ducle@ducs-MacBook-Pro.local>
2022-05-14 15:10:25 -0700

    first commit

---
+++ src/RecipeCreate.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+
+function RecipeCreate({ recipes, setRecipes }) {
+  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
+  // TODO: Add the required input and textarea form elements.
+  // TODO: Add the required submit and change handlers
+  const initRecipe = {
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "",
+    preparation: "",
+  };
+
+  const [userInput, setUserInput] = useState({ ...initRecipe });
+
+  const onChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setRecipes([...recipes, userInput]);
+    setUserInput({ ...initRecipe });
+  };
+
+  return (
+    <>
+      <form name="create">
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <label htmlFor="name"></label>
+                <input
+                  onChange={(event) => onChangeHandler(event)}
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={userInput.name}
+                  placeholder="Name"
+                />
+              </td>
+              <td>
+                <label htmlFor="cuisine"></label>
+                <input
+                  onChange={(event) => onChangeHandler(event)}
+                  type="text"
+                  id="cuisine"
+                  name="cuisine"
+                  value={userInput.cuisine}
+                  placeholder="Cuisine"
+                />
+              </td>
+              <td>
+                <label htmlFor="photo"></label>
+                <input
+                  onChange={(event) => onChangeHandler(event)}
+                  type="text"
+                  id="photo"
+                  name="photo"
+                  value={userInput.photo}
+                  placeholder="URL"
+                />
+              </td>
+              <td>
+                <label htmlFor="ingredients"></label>
+                <textarea
+                  onChange={(event) => onChangeHandler(event)}
+                  id="ingredients"
+                  name="ingredients"
+                  value={userInput.ingredients}
+                  placeholder="Ingredients"
+                ></textarea>
+              </td>
+              <td>
+                <label htmlFor="preparation"></label>
+                <textarea
+                  onChange={(event) => onChangeHandler(event)}
+                  id="preparation"
+                  name="preparation"
+                  value={userInput.preparation}
+                  placeholder="Preparation"
+                ></textarea>
+              </td>
+              <td>
+                <button onClick={(event) => submitHandler(event)} type="submit">
+                  Create
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    </>
+  );
+}
+
+export default RecipeCreate;
---
+++ src/RecipeList.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+function RecipeList({ recipes, setRecipes }) {
+  // TODO: Display the list of recipes using the structure of table that is provided.
+  // TODO: Create at least one additional component that is used by this component.
+  // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
+  const deleteRecipe = (item) => {
+    const updateRecipes = recipes.filter((recipe) => item.name !== recipe.name);
+    setRecipes([...updateRecipes]);
+  };
+
+  const recipeList = recipes.map((item, index) => {
+    return (
+      <tr key={index}>
+        <td>{item.name}</td>
+        <td>{item.cuisine}</td>
+        <td>
+          <img src={item.photo} alt="food" />
+        </td>
+        <td className="content_td">
+          <p>{item.ingredients}</p>
+        </td>
+        <td className="content_td">
+          <p>{item.preparation}</p>
+        </td>
+        <td>
+          <button onClick={() => deleteRecipe(item)} name="delete">
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <div className="recipe-list">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Cuisine</th>
+            <th>Photo</th>
+            <th>Ingredients</th>
+            <th>Preparation prep</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>{recipeList}</tbody>
+      </table>
+    </div>
+  );
+}
+
+export default RecipeList;
