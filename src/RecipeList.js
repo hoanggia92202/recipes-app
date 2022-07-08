b8bf076f67a397a2b5f4d0a71775fc29bd69ed5b
duc le <ducle@ducs-MacBook-Pro.local>
2022-07-07 18:39:13 -0700

    remove comments

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,9 +1,6 @@
 import React from "react";
 
 function RecipeList({ recipes, setRecipes }) {
-  // TODO: Display the list of recipes using the structure of table that is provided.
-  // TODO: Create at least one additional component that is used by this component.
-  // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
   const deleteRecipe = (item) => {
     const updateRecipes = recipes.filter((recipe) => item.name !== recipe.name);
     setRecipes([...updateRecipes]);
